bf76219dfda8709e0e9481f2d2bcd30c4672cc11
sgodfrey66 <stephen_godfrey@ymail.com>
2024-12-27 14:08:06 -0800

    gcp_test2

---
+++ code/sprints/gcp_sl_container/app.py
@@ -0,0 +1,88 @@
+# [2412]
+#
+import os
+
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import OllamaLLM
+
+import streamlit as st
+import numpy as np
+
+import json
+
+st.title("CCC Bot : 2412")
+
+succinct_txt = ("Please provide only a one or two word answer. " +
+                "Be as succinct as possible when answering. ")
+initial_question = "What shall we discuss?"
+
+
+with st.sidebar:
+    request_succinct = st.checkbox("Request succinct")
+    st.text('The Request Succinct option appends this text to the prompt : "' +
+             succinct_txt + '"')
+    if st.button("Save Transcript"):
+        print("save transcript")
+        messages = []
+        for i in range(len(st.session_state.messages)):
+            message = st.session_state.messages[i].copy()
+            j = int(i/2)
+            message["succinct"] = st.session_state.succinct_hist[j]
+            print(message)
+            messages.append(message)
+        with open('ccc_bot-transcript.json', 'w') as file:
+            json.dump(messages, file)
+
+
+st.text("Example question : What is the California community college with the largest student enrollment?")
+st.text("Note : make sure the Ollama client is running while using this application.")
+
+st.divider()
+
+os.environ["OLLAMA_HOST"] = "https://ccc-polasst-1062597788108.us-central1.run.app"
+llm = OllamaLLM(model="phi3.5")
+
+template = """
+Question: {question}
+Answer: 
+"""
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+chain = prompt | llm
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    st.session_state.succinct_hist = []
+
+# displays the chat history when app is rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input(initial_question):
+
+    st.chat_message("user").markdown(prompt)
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        if request_succinct:
+            prompt += " " + succinct_txt
+            print(prompt)
+            st.session_state.succinct_hist.append("True")
+        else:
+            st.session_state.succinct_hist.append("False")
+        result = chain.invoke(prompt)
+
+        st.markdown(result)
+
+    st.session_state.messages.append({"role": "assistant", "content": result})
+
+
+# [\] add succinct setting to transcript
+# [x] json formatted transcript
+# [ ] data collection on impact of succinct option (word counts with and without)
+# [ ] other options for the sidebar / chat session
+#
