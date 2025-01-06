3e5c311f8615d86c378dba7ae221a65c654295e7
sgodfrey66 <stephen_godfrey@ymail.com>
2025-01-06 11:59:16 -0800

    gcp_test18

--- code/sprints/gcp_sl2_container/rag_bot_1.py
+++ code/sprints/gcp_sl2_container/rag_bot_1.py
@@ -1,31 +1,35 @@
-##########
-# Code for the chatbot intended to be run using Streamlit
-
-from langchain_core.tools import tool
-from langgraph.graph import MessagesState, StateGraph
-from langchain_core.messages import SystemMessage
-from langgraph.prebuilt import ToolNode
-
-from langchain_chroma import Chroma
-from langchain_google_vertexai import VertexAI, VertexAIEmbeddings, ChatVertexAI
-
-from langgraph.graph import END
-from langgraph.prebuilt import ToolNode, tools_condition
-
-import vertexai
+############
+# Experimental code creating a LangCHain RAG Bot class
 
 import sys, os
+
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+from langgraph.graph import START, StateGraph
+from langchain import hub
+from langchain_google_vertexai import VertexAI, VertexAIEmbeddings, ChatVertexAI
+import vertexai
+from langchain_chroma import Chroma
+
 sys.path.insert(0, "../../utils")
 import gcp_tools as gct
 import authentication as auth
 
+
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+
 class CCCPolicyAssistant:
-    """ BotCCCGlobals class holds globals for a chatbot based on a
-    pre-trained model (phi3) and document retrieval from a vector
-    database (ChromaDB). It includes a range of defaults, including
-    an (optional) succinctness constraint, the default question, the
-    file name for any saved transcripts, and the path & collection
-    name for the vector database. """
+    '''
+    First draft of a CCC Policy Policy Assistant Chatbot using
+    LangChain components
+
+    Reference: https://python.langchain.com/docs/tutorials/rag/
+    '''
 
     def __init__(self):
 
@@ -42,9 +46,9 @@ class CCCPolicyAssistant:
         self.embedding_model = "textembedding-gecko@003"
         # self.embedding_model = "text-embedding-004"
         self.embedding_num_batch = 5
-        # self.embeddings_local_path = "./local_chromadb/"
-        self.embeddings_local_path = ("/Users/stephengodfrey/OneDrive - numanticsolutions.com"
-                                      "/Engagements/Projects/ccc_policy_assistant/data/embeddings-vai")
+        self.embeddings_local_path = "./local_chromadb/"
+        # self.embeddings_local_path = ("/Users/stephengodfrey/OneDrive - numanticsolutions.com"
+        #                               "/Engagements/Projects/ccc_policy_assistant/data/embeddings-vai")
 
         self.llm_model = "gemini-1.5-pro"
         # self.llm_model = "gemini-1.5-flash"
@@ -104,9 +108,11 @@ class CCCPolicyAssistant:
         ### Step 4. Establish an embeddings model
         self.vector_store = self.setup_vectorstore()
 
+        ### Step 5. Set up prompt
+        self.prompt = hub.pull("rlm/rag-prompt")
+
         ### Step 5. Establish an embeddings model
         self.graph = self.setup_conversation_graph()
-
 
     def setup_vectorstore(self):
         '''
@@ -119,7 +125,7 @@ class CCCPolicyAssistant:
         # Download files from GCP
         gct.download_directory_from_gcs(gcs_project_id=self.gcp_project_id,
                                         gcs_bucket_name=self.gcs_embeddings_bucket_name,
-                                        gcs_directory="",
+                                        gcs_directory=self.gcs_embeddings_directory,
                                         local_directory=self.embeddings_local_path)
 
         # Load embeddings and persisted data
@@ -131,90 +137,20 @@ class CCCPolicyAssistant:
                     embedding_function=embeddings)
         return db
 
+    # Define application steps
+    def retrieve(self, state: State):
+        retrieved_docs = self.vector_store.similarity_search(state["question"])
+        return {"context": retrieved_docs}
 
-    @tool(response_format="content_and_artifact")
-    def retrieve(self, query: str):
-        """Retrieve information related to a query."""
-        retrieved_docs = self.vector_store.similarity_search(query,
-                                                        k=self.doc_search_retrieval_k)
-
-        serialized = "\n\n".join(
-            (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
-            for doc in retrieved_docs
-        )
-        return serialized, retrieved_docs
-
-
-    def query_or_respond(self, state: MessagesState):
-        '''
-        # Step 1: Generate an AIMessage that may include a tool-call to be sent.
-        Generate tool call for retrieval or respond."
-        '''
-        llm_with_tools = self.llm.bind_tools([self.retrieve])
-        response = llm_with_tools.invoke(state["messages"])
-        # MessagesState appends messages to state instead of overwriting
-        return {"messages": [response]}
-
-
-    def generate(self, state: MessagesState):
-        '''
-        Generate answer.# Step 3: Generate a response using the retrieved content.
-        '''
-        # Get generated ToolMessages
-        recent_tool_messages = []
-        for message in reversed(state["messages"]):
-            if message.type == "tool":
-                recent_tool_messages.append(message)
-            else:
-                break
-        tool_messages = recent_tool_messages[::-1]
-
-        # Format into prompt
-        docs_content = "\n\n".join(doc.content for doc in tool_messages)
-        system_message_content = (f"{self.prompt_template}"
-                                  "\n\n"
-                                  f"{docs_content}")
-
-        conversation_messages = [
-            message
-            for message in state["messages"]
-            if message.type in ("human", "system")
-            or (message.type == "ai" and not message.tool_calls)
-        ]
-        prompt = [SystemMessage(system_message_content)] + conversation_messages
-
-        # Run
-        response = self.llm.invoke(prompt)
-        return {"messages": [response]}
-
+    def generate(self, state: State):
+        docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+        messages = self.prompt.invoke({"question": state["question"], "context": docs_content})
+        response = self.llm.invoke(messages)
+        return {"answer": response.content}
 
     def setup_conversation_graph(self):
-        '''
-        Set up a Streamlit conversational graph
-
-        :return:
-        '''
-
-        ## Step 12: Create graph_builder
-        graph_builder = StateGraph(MessagesState)
-
-        # Step 14: Execute the retrieval.
-        tools = ToolNode([self.retrieve])
-
-        # Step 16: Build graph
-        graph_builder.add_node(self.query_or_respond)
-        graph_builder.add_node(tools)
-        graph_builder.add_node(self.generate)
-
-        graph_builder.set_entry_point("query_or_respond")
-        graph_builder.add_conditional_edges(
-            "query_or_respond",
-            tools_condition,
-            {END: END, "tools": "tools"},
-        )
-        graph_builder.add_edge("tools", "generate")
-        graph_builder.add_edge("generate", END)
-
+        # Compile application and test
+        graph_builder = StateGraph(State).add_sequence([self.retrieve, self.generate])
+        graph_builder.add_edge(START, "retrieve")
         graph = graph_builder.compile()
-
         return graph
